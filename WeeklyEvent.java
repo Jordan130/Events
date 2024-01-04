f7063aa30a706be3ceb3f256674f0a5955c86a98
Jordan Rios <145077985+Jordan130@users.noreply.github.com>
2024-01-03 22:59:29 -0500

    WeeklyEvent

--- WeeklyEvent.java
+++ WeeklyEvent.java
@@ -51,6 +51,7 @@ public class WeeklyEvent extends RepeatingEvent {
 
 	}
 
+	// Returns a String representaion of this object.
 	@Override
 	public String toString() {
 		if (super.getRepetitions() == 0) {
